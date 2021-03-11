9b51eac343f1be50936fd0d86b9bd7c31e3967c6
Michele Simioli <michele@simioli.it>
2021-03-11 11:42:57 +0100

    added library file

---
+++ matem.go
@@ -0,0 +1,30 @@
+package matem
+
+// Doppio ritorna il doppio di un intero
+func Doppio(a int) int {
+	return 2 * a
+}
+
+// Triplo ritorna il triplo di un intero
+func Triplo(a int) int {
+	return 3 * a
+}
+
+// Massimo : il numero massimo trattato
+const Massimo int = 100
+
+// Meta ritorna la metà di un intero come float32
+// purchè l'intero sia minore del Massimo
+func Meta(a int) float32 {
+	if check(a) {
+		return float32(a) / 2.0
+	}
+	return float32(0.0)
+}
+
+func check(x int) bool {
+	if x <= Massimo {
+		return true
+	}
+	return false
+}
